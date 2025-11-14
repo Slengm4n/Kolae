d9176356e15956b9d539652eabccb02377147547
João Slengman <jpcslengman@outlook.com>
2025-11-14 19:04:18 -0300

    update colae_sql

--- database/colae_db.sql
+++ database/colae_db.sql
@@ -3,218 +3,295 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 23, 2025 at 02:55 AM
+-- Generation Time: Nov 14, 2025 at 11:03 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
-SET
-  SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
+SET time_zone = "+00:00";
 
-SET
-  time_zone = "+00:00";
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-
 /*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `colae_db`
 --
+
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `addresses`
 --
-CREATE TABLE
-  `addresses` (
-    `id` int (11) NOT NULL,
-    `cep` varchar(9) DEFAULT NULL,
-    `street` varchar(100) DEFAULT NULL,
-    `number` varchar(10) DEFAULT NULL,
-    `neighborhood` varchar(50) DEFAULT NULL,
-    `complement` varchar(100) DEFAULT NULL,
-    `city` varchar(50) DEFAULT NULL,
-    `state` varchar(2) DEFAULT NULL,
-    `latitude` decimal(10, 8) DEFAULT NULL,
-    `longitude` decimal(11, 8) DEFAULT NULL
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `addresses` (
+  `id` int(11) NOT NULL,
+  `cep` varchar(9) DEFAULT NULL,
+  `street` varchar(100) DEFAULT NULL,
+  `number` varchar(10) DEFAULT NULL,
+  `neighborhood` varchar(50) DEFAULT NULL,
+  `complement` varchar(100) DEFAULT NULL,
+  `city` varchar(50) DEFAULT NULL,
+  `state` varchar(2) DEFAULT NULL,
+  `latitude` decimal(10,8) DEFAULT NULL,
+  `longitude` decimal(11,8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `matches`
+--
+
+CREATE TABLE `matches` (
+  `id` int(11) NOT NULL,
+  `venue_id` int(11) NOT NULL,
+  `sport_id` int(11) NOT NULL,
+  `creator_user_id` int(11) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `duration_minutes` int(11) DEFAULT 60,
+  `status` enum('scheduled','completed','canceled') NOT NULL DEFAULT 'scheduled',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `password_resets`
 --
-CREATE TABLE
-  `password_resets` (
-    `id` int (11) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `token` varchar(255) NOT NULL,
-    `expires_at` datetime NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `password_resets` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `expires_at` datetime NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `sports`
 --
-CREATE TABLE
-  `sports` (
-    `id` int (11) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `status` enum ('active', 'inactive') NOT NULL,
-    `icon` varchar(255) DEFAULT NULL
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `sports` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` enum('active','inactive') NOT NULL,
+  `icon` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `users`
 --
-CREATE TABLE
-  `users` (
-    `id` int (11) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `birthdate` date NOT NULL,
-    `password_hash` varchar(255) NOT NULL,
-    `role` enum ('user', 'admin') NOT NULL DEFAULT 'user',
-    `status` enum ('active', 'inactive') NOT NULL DEFAULT 'active',
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    `cpf` varchar(255) DEFAULT NULL,
-    `avatar_path` varchar(255) DEFAULT NULL,
-    `force_password_change` tinyint (1) NOT NULL DEFAULT 1
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `birthdate` date NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `role` enum('user','admin') NOT NULL DEFAULT 'user',
+  `status` enum('active','inactive') NOT NULL DEFAULT 'active',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `cnpj` varchar(255) DEFAULT NULL,
+  `avatar_path` varchar(255) DEFAULT NULL,
+  `force_password_change` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `venues`
 --
-CREATE TABLE
-  `venues` (
-    `id` int (11) NOT NULL,
-    `user_id` int (11) NOT NULL,
-    `address_id` int (11) NOT NULL,
-    `name` varchar(255) NOT NULL,
-    `average_price_per_hour` decimal(10, 2) DEFAULT NULL,
-    `court_capacity` int (11) DEFAULT NULL,
-    `has_leisure_area` tinyint (1) NOT NULL,
-    `leisure_area_capacity` int (11) DEFAULT NULL,
-    `floor_type` enum (
-      'grama natural',
-      'grama sintética',
-      'cimento',
-      'bosta'
-    ) DEFAULT NULL,
-    `has_lighting` tinyint (1) NOT NULL,
-    `is_covered` tinyint (1) NOT NULL DEFAULT 0,
-    `status` enum ('available', 'unavailable', 'in_maintenance') NOT NULL DEFAULT 'available',
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-    `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `venues` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `average_price_per_hour` decimal(10,2) DEFAULT NULL,
+  `court_capacity` int(11) DEFAULT NULL,
+  `has_leisure_area` tinyint(1) NOT NULL,
+  `leisure_area_capacity` int(11) DEFAULT NULL,
+  `floor_type` enum('grama natural','grama sintética','cimento','bosta') DEFAULT NULL,
+  `has_lighting` tinyint(1) NOT NULL,
+  `is_covered` tinyint(1) NOT NULL DEFAULT 0,
+  `status` enum('available','unavailable','in_maintenance') NOT NULL DEFAULT 'available',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
+
 --
 -- Table structure for table `venue_images`
 --
-CREATE TABLE
-  `venue_images` (
-    `id` int (11) NOT NULL,
-    `venue_id` int (11) NOT NULL,
-    `file_path` varchar(255) NOT NULL,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+
+CREATE TABLE `venue_images` (
+  `id` int(11) NOT NULL,
+  `venue_id` int(11) NOT NULL,
+  `file_path` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `venue_sports`
+--
+
+CREATE TABLE `venue_sports` (
+  `venue_id` int(11) NOT NULL,
+  `sport_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
 --
+
 --
 -- Indexes for table `addresses`
 --
-ALTER TABLE `addresses` ADD PRIMARY KEY (`id`);
+ALTER TABLE `addresses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `match_venue_fk` (`venue_id`),
+  ADD KEY `match_sport_fk` (`sport_id`),
+  ADD KEY `match_creator_fk` (`creator_user_id`);
 
 --
 -- Indexes for table `password_resets`
 --
-ALTER TABLE `password_resets` ADD PRIMARY KEY (`id`);
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `sports`
 --
-ALTER TABLE `sports` ADD PRIMARY KEY (`id`);
+ALTER TABLE `sports`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
-ALTER TABLE `users` ADD PRIMARY KEY (`id`),
-ADD UNIQUE KEY `cpf` (`cpf`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `cpf` (`cnpj`);
 
 --
 -- Indexes for table `venues`
 --
-ALTER TABLE `venues` ADD PRIMARY KEY (`id`),
-ADD KEY `user_id` (`user_id`),
-ADD KEY `address_id` (`address_id`);
+ALTER TABLE `venues`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `address_id` (`address_id`);
 
 --
 -- Indexes for table `venue_images`
 --
-ALTER TABLE `venue_images` ADD PRIMARY KEY (`id`),
-ADD KEY `venue_id` (`venue_id`);
+ALTER TABLE `venue_images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `venue_id` (`venue_id`);
+
+--
+-- Indexes for table `venue_sports`
+--
+ALTER TABLE `venue_sports`
+  ADD PRIMARY KEY (`venue_id`,`sport_id`),
+  ADD KEY `sport_id` (`sport_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
 --
 -- AUTO_INCREMENT for table `addresses`
 --
-ALTER TABLE `addresses` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `addresses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `password_resets`
 --
-ALTER TABLE `password_resets` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `password_resets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `sports`
 --
-ALTER TABLE `sports` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `sports`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `users` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `venues`
 --
-ALTER TABLE `venues` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `venues`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `venue_images`
 --
-ALTER TABLE `venue_images` MODIFY `id` int (11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `venue_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `matches`
+--
+ALTER TABLE `matches`
+  ADD CONSTRAINT `match_creator_fk` FOREIGN KEY (`creator_user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `match_sport_fk` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `match_venue_fk` FOREIGN KEY (`venue_id`) REFERENCES `venues` (`id`) ON DELETE CASCADE;
+
 --
 -- Constraints for table `venues`
 --
-ALTER TABLE `venues` ADD CONSTRAINT `venues_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-ADD CONSTRAINT `venues_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`) ON DELETE CASCADE;
+ALTER TABLE `venues`
+  ADD CONSTRAINT `venues_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `venues_ibfk_2` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `venue_images`
 --
-ALTER TABLE `venue_images` ADD CONSTRAINT `venue_images_ibfk_1` FOREIGN KEY (`venue_id`) REFERENCES `venues` (`id`) ON DELETE CASCADE;
+ALTER TABLE `venue_images`
+  ADD CONSTRAINT `venue_images_ibfk_1` FOREIGN KEY (`venue_id`) REFERENCES `venues` (`id`) ON DELETE CASCADE;
 
+--
+-- Constraints for table `venue_sports`
+--
+ALTER TABLE `venue_sports`
+  ADD CONSTRAINT `venue_sports_ibfk_1` FOREIGN KEY (`venue_id`) REFERENCES `venues` (`id`),
+  ADD CONSTRAINT `venue_sports_ibfk_2` FOREIGN KEY (`sport_id`) REFERENCES `sports` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
